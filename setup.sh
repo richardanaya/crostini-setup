c4e9bed9711872e60f9d637082b0297a3d88b1df
Richard Anaya <richard.anaya@gmail.com>
2018-05-12 16:56:18 +0000

    adding first setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+sudo apt-get install wget
+wget https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
+tar -C ~/ -xzf go1.10.2.linux-amd64.tar.gz
+rm go1.10.2.linux-amd64.tar.gz
+echo 'export PATH="$HOME/go/bin:$PATH"' >> ~/.profile 
+source ~/.profile
