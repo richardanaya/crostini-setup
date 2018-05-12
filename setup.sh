99a3abbfddae90ff7bf916017fa740d826a19610
Richard Anaya <richard.anaya@gmail.com>
2018-05-12 16:57:53 +0000

    use .go path

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 sudo apt-get install wget
 wget https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
 tar -C ~/ -xzf go1.10.2.linux-amd64.tar.gz
+mv ~/go ~/.go
 rm go1.10.2.linux-amd64.tar.gz
-echo 'export PATH="$HOME/go/bin:$PATH"' >> ~/.profile 
+echo 'export PATH="$HOME/.go/bin:$PATH"' >> ~/.profile 
 source ~/.profile
